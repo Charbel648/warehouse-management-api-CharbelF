d283cd5e42633eaf3b0abb5da363d17e9dabf52e
charbel648 <charbel.faddoul@net.usj.edu.lb>
2026-07-11 14:59:47 +0300

    feat(session-04): configure code first database connection

--- Warehouse.Presentation/Program.cs
+++ Warehouse.Presentation/Program.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Warehouse.Application.Mapping;
 using Warehouse.Application.Products.Commands.CreateProduct;
 using Warehouse.Domain.Repositories;
-using Warehouse.Infrastructure.Persistence.DbFirst;
+using Warehouse.Infrastructure.Persistence;
 using Warehouse.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,18 +12,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<IProductRepository, ProductRepository>();
-builder.Services.AddSingleton<ISupplierRepository, SupplierRepository>();
-
-builder.Services.AddDbContext<WarehouseDbFirstContext>(options =>
+builder.Services.AddDbContext<WarehouseDbContext>(options =>
 {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("WarehouseDbFirst"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("WarehouseDb"));
 });
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ISupplierRepository, SupplierRepository>();
 
 builder.Services.AddMediatR(cfg =>
 {
     cfg.RegisterServicesFromAssembly(typeof(CreateProductCommand).Assembly);
 });
+
+builder.Services.AddAutoMapper(typeof(WarehouseProfile).Assembly);
 
 var app = builder.Build();
 
