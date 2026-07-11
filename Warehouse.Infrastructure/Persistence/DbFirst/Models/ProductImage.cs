4e5e3f29ed1489782d88702f8353151f82bfec61
charbel648 <charbel.faddoul@net.usj.edu.lb>
2026-07-11 13:37:41 +0300

    feat(session-04): db first - scaffolding

---
+++ Warehouse.Infrastructure/Persistence/DbFirst/Models/ProductImage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Warehouse.Infrastructure.Persistence.DbFirst.Models;
+
+public partial class ProductImage
+{
+    public Guid ProductImageId { get; set; }
+
+    public Guid ProductId { get; set; }
+
+    public string FileName { get; set; } = null!;
+
+    public string FilePath { get; set; } = null!;
+
+    public DateTime UploadedAt { get; set; }
+
+    public virtual Product Product { get; set; } = null!;
+}
