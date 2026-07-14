353566b4244eeee00abcbab3e4703a4948c2adb6
charbel648 <charbel.faddoul@net.usj.edu.lb>
2026-07-14 18:04:59 +0300

    feat(session-05): register middleware and mvc filters

--- Warehouse.Presentation/Program.cs
+++ Warehouse.Presentation/Program.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Warehouse.Application.Mapping;
 using Warehouse.Application.Products.Commands.CreateProduct;
 using Warehouse.Domain.Repositories;
 using Warehouse.Infrastructure.Persistence;
 using Warehouse.Infrastructure.Repositories;
+using Warehouse.Presentation.Filters;
+using Warehouse.Presentation.Middleware;
+using Warehouse.Presentation.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
+
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<ModelValidationFilter>();
+    options.Filters.Add<ActionLoggingFilter>();
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,9 +30,17 @@ builder.Services.AddDbContext<WarehouseDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("WarehouseDb"));
 });
 
+builder.Services.AddDbContextFactory<WarehouseDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("WarehouseDb"));
+});
+
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ISupplierRepository, SupplierRepository>();
-builder.Services.AddScoped<IProductReportRepository, ProductReportRepository>();
+
+builder.Services.AddScoped<ModelValidationFilter>();
+builder.Services.AddScoped<ActionLoggingFilter>();
+builder.Services.AddScoped<ValidationMetadataService>();
 
 builder.Services.AddMediatR(cfg =>
 {
@@ -32,6 +53,10 @@ builder.Services.AddAutoMapper(config =>
 });
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+app.UseMiddleware<CorrelationIdMiddleware>();
+app.UseMiddleware<RequestTimingMiddleware>();
 
 app.UseSwagger();
 app.UseSwaggerUI();
