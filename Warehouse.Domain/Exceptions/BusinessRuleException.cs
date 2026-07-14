5c9dae2ea486efbac2d2f973e16a3a5e9409ff83
charbel648 <charbel.faddoul@net.usj.edu.lb>
2026-07-14 18:01:57 +0300

    feat(session-05): add custom exceptions

---
+++ Warehouse.Domain/Exceptions/BusinessRuleException.cs
@@ -0,0 +1,14 @@
+﻿namespace Warehouse.Domain.Exceptions;
+
+public class BusinessRuleException : Exception
+{
+    public string ErrorCode { get; }
+
+    public BusinessRuleException(
+        string message,
+        string errorCode = "business_rule_violation")
+        : base(message)
+    {
+        ErrorCode = errorCode;
+    }
+}
---
+++ Warehouse.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,15 @@
+﻿namespace Warehouse.Domain.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public string ResourceName { get; }
+
+    public string ResourceId { get; }
+
+    public NotFoundException(string resourceName, string resourceId)
+        : base($"{resourceName} with id '{resourceId}' was not found")
+    {
+        ResourceName = resourceName;
+        ResourceId = resourceId;
+    }
+}
